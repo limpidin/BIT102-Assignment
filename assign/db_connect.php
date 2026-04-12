a1388bcb3c35214cd2096c4cfd1f389409259445
lvmiao061015-del <lvmiao061015@gmail.com>
2026-04-13 03:58:11 +1000

    Add database connection setup in db_connect.php

--- assign/db_connect.php
+++ assign/db_connect.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "innercircle";
 
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+?>
